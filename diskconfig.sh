6ac95b12b1172b60d991cc349c3f5198b5201ed5
root user <root@domain.com>
2024-03-05 16:09:18 -0500

    First commit

---
+++ diskconfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+if [ "$1" = "cool" ] then echo "Cool Beans”
+  elif [ "$1" = "neat" ] then echo "Neato Cool”
+ else echo "Not Cool Beans" 
+fi
+
+## $1 is the first word on the command line
+## after the command itself
+echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
+mdadm --detail /dev/md0 >>$FILENAME.report
